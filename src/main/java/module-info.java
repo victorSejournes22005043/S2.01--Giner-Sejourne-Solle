406be95d125e36744e05c8bf745ed7ab692de20f
victorSejournes22005043 <115543123+victorSejournes22005043@users.noreply.github.com>
2023-06-05 15:04:19 +0200

    Add files via upload

---
+++ src/main/java/module-info.java
@@ -0,0 +1,10 @@
+module com.example.demo {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+    requires org.controlsfx.controls;
+    requires org.kordamp.bootstrapfx.core;
+
+    opens com.example.s201 to javafx.fxml;
+    exports com.example.s201;
+}
