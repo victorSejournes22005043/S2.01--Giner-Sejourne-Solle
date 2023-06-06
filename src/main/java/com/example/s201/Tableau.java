589cab2651a6b556f87f2d5fa297705fdb0e764b
victorsejourne <victor.sejourne@etu.amu-univ.fr>
2023-06-06 09:17:59 +0200

    creation de la class tableau et ajout d'un fichier csv

---
+++ src/main/java/com/example/s201/Tableau.java
@@ -0,0 +1,16 @@
+package com.example.s201;
+
+import java.util.ArrayList;
+import java.io.BufferedReader;
+
+public class Tableau {
+    private static ArrayList<Integer> tableau;
+
+    public void initTab() {
+        //tableau = ("books.txt");
+    }
+
+    public ArrayList<Integer> getTableau() {
+        return tableau;
+    }
+}
