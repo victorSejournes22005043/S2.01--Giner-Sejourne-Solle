e3dbc91b2c0d4163648a0270f17798f6f58d97d5
victorsejourne <victor.sejourne@etu.amu-univ.fr>
2023-06-13 09:22:28 +0200

    opencsv

--- src/main/java/com/example/s201/CSVManager.java
+++ src/main/java/com/example/s201/CSVManager.java
@@ -2,21 +2,14 @@ package com.example.s201;
 
 import java.io.*;
 
-import com.opencsv.CSVParserBuilder;
 import com.opencsv.CSVReader;
-import com.opencsv.CSVReaderBuilder;
 import com.opencsv.exceptions.CsvValidationException;
 
 import java.io.FileReader;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
-
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
-import java.util.Date;
 
 
 public class CSVManager {
